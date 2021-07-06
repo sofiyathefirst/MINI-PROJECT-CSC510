a9cdb81a2fdd3030f2f8a3aed0c0d1069fa67d01
Sarah Sofiya <69139655+sofiyathefirst@users.noreply.github.com>
2021-07-06 11:18:57 +0800

    Add files via upload

--- MINI PROJECT FSM/FSM CLANG.c
+++ MINI PROJECT FSM/FSM CLANG.c
@@ -1,14 +1,27 @@
 int main()
 {
     enum{S0, S1, S2, S3, S4, Sstop} state;
+    char input;
 
     state = S0;
     while(state != Sstop)
     {
         switch(state)
         {
-            case S0;
-            
+            case S0:
+            if(input == 'a') {state = S1;}
+            break;
+            case S1:
+            if(input == 'b') {state = S3;}
+            break;
+            case S2:
+            if(input == 'a') {state = S3;}
+            if(input == 'b') {state = S4;}
+            break;
+            case S3:
+            if(input == 'a') {state = S4;}
+            break;
         }
     }
+    return 0;
 }
